5caf4a54026295128df27436dcbe92ed0ab66415
MihirJoshiB <98804755+MihirJoshiB@users.noreply.github.com>
2022-03-30 21:16:42 +0530

    commit

--- server.js
+++ server.js
@@ -5,5 +5,11 @@ dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+app.get('/', (req, res) => {
+
+    res.json({name :'Mihir'});
+    
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
