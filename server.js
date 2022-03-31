98f3e905f634466c1d150607dc959d6ca381c3f7
MihirJoshiB <98804755+MihirJoshiB@users.noreply.github.com>
2022-03-31 12:38:12 +0530

    commit

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//route files  
+const bootcamps = require('./routes/Bootecamps');
 dotenv.config({ path: './config/config.env'});
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-
-    res.json({name :'Mihir'});
-    
-});
+//mount routers
+app.use('/',bootcamps)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
