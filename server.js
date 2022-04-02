1436580c29563c1b03b934347e9a7a4d1f7a3080
MihirJoshiB <98804755+MihirJoshiB@users.noreply.github.com>
2022-04-02 21:15:59 +0530

    commit

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+//const logger = require('./middleware/logger');
+const morgan = require('morgan');
 //route files  
 const bootcamps = require('./routes/Bootecamps');
+
+const res = require('express/lib/response');
 dotenv.config({ path: './config/config.env'});
 
 
 const app = express();
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development')
+{
+    app.use(morgan('dev'));
+}
+
 
 //mount routers
 app.use('/',bootcamps)
